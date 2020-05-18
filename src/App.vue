89c203d10ffa19b85c7f02995bf9839a2b2d2c68
GaoCarri <gaocarri@163.com>
2020-05-18 22:22:48 +0800

    初始化5个标签

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,18 @@
 
 <script>
 export default {
-  name: "App"
+  name: "App",
+  mounted() {
+    console.log(JSON.parse(window.localStorage.getItem("tagList")).length);
+    if (JSON.parse(window.localStorage.getItem("tagList")).length === 0) {
+      this.$store.commit("createTag", { name: "餐饮", id: 1 });
+      this.$store.commit("createTag", { name: "购物", id: 7 });
+      this.$store.commit("createTag", { name: "交通", id: 14 });
+      this.$store.commit("createTag", { name: "居住", id: 21 });
+      this.$store.commit("createTag", { name: "娱乐", id: 25 });
+      this.$store.commit("createTag", { name: "医疗", id: 30 });
+    }
+  }
 };
 </script>>
 
