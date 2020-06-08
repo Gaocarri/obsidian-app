41826c9fb8a40b88118b85499901de5115dd8c7d
GaoCarri <gaocarri@163.com>
2020-06-08 22:55:45 +0800

    添加了二维码

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div id="app">
+    <div id="qrcode" v-if="showQrCode">
+      <img src="./assets/qrcode-money.png" />
+      <div>请打开手机浏览器扫描二维码或打开调试模式模拟手机并刷新，以保证浏览效果</div>
+    </div>
     <router-view />
   </div>
 </template>
@@ -7,7 +11,15 @@
 <script>
 export default {
   name: "App",
+  data() {
+    return {
+      showQrCode: false
+    };
+  },
   mounted() {
+    if (document.documentElement.clientWidth > 500) {
+      this.showQrCode = true;
+    }
     // 初始化六个标签
     if (window.localStorage.getItem("tagList") == null) {
       this.$store.commit("createTag", { name: "餐饮", id: 1 });
@@ -31,6 +43,16 @@ export default {
   line-height: 1.5;
   font-family: $font-hei;
   font-size: 16px;
+}
+
+#qrcode {
+  z-index: 10;
+  position: fixed;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  font-size: 30px;
 }
 </style>
 
